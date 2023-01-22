5165727e82e323e50c9ea0b59120089955975191
Yazeed <Yazeedrq9@gmail.com>
2023-01-22 17:44:00 +0300

    lab review2

---
+++ classes.py
@@ -0,0 +1,59 @@
+class Programmer:
+    Type = 'Programmer'
+    def __init__(self,name:str,programmerID:int,programmingLanguage:str):
+    
+        self.__name = name
+        self.__programmingLanguage = programmingLanguage
+        self.__programmerID = programmerID
+
+    def set_name(self,name:str):
+        self.__name = name.upper()
+    def get_name(self):
+        return self.__name
+
+    def set_programmerID(self,programmerID:int):
+        if type(programmerID) != int:
+            raise TypeError('ID Type is Wrong')
+
+        self.__programmerID = programmerID
+    def get_programmerID(self):
+        return self.__programmerID
+    
+    def set_programmingLanguage(self,programmingLanguage:str):
+        self.__programmingLanguage = programmingLanguage
+    def get_programmingLanguage(self):
+        return self.__programmingLanguage
+
+    #Methods
+    def write_info(self):
+        programmer_file_info=open('programmer_info.txt','a+',encoding='utf-8')
+        programmer_file_info.write(f'Programmer Name is : {self.get_name()} \t\t Programmer ID is : {self.get_programmerID()} \t\t Programming Language is : {self.get_programmingLanguage()} \n')
+        programmer_file_info.close()
+        return f'Programmer information has been recorded'
+
+    def print_info(self):
+        return f'Programmer Name is : {self.get_name()} \t\t Programmer ID is : {self.get_programmerID()} \t\t Programming Language is : {self.get_programmingLanguage()} '
+
+     
+##########################################################################################
+
+class Student(Programmer):
+    Type='Student'
+    def __init__(self, name: str, studentID:int, programmingLanguage: str):
+        super().__init__(name, studentID, programmingLanguage)
+        self.__name = name
+        self.__studentID = studentID
+        self.__programmingLanguage = programmingLanguage
+    
+    def set_studentID(self,studentID):
+        if type(studentID) != int:
+            raise TypeError('ID Type is Wrong')
+        self.__studentID = studentID
+    def get_studentID(self):
+        return self.__studentID
+
+    def write_info(self):
+        student_file_info=open('student_info.txt','a+',encoding='utf-8')
+        student_file_info.write(f'Student Name is : {self.get_name()} \t\t Student ID is : {self.get_studentID()} \t\t Programming Language is : {self.get_programmingLanguage()} \n')
+        student_file_info.close()
+        return f'student information has been recorded'
---
+++ main.py
@@ -0,0 +1,31 @@
+from classes import Programmer, Student
+
+def read_files(file_name):
+    try:
+        with open(file_name, "r") as file:
+            content = file.read()
+            return content
+    except:
+        print("An error occured while reading the file")
+        return None
+
+programmer1=Programmer('Mohammed',123,'java')
+programmer1.write_info()
+print(programmer1.print_info())
+
+student1=Student('Yazeed',12,'JS')
+student1.write_info()
+print(student1.print_info())
+
+print(read_files('student'))
+
+try:
+    student1.__studentID = "Not an int"
+except TypeError as e:
+    print("An error occured:", e)
+
+try:
+    #just write file name with out extend
+    read_files('student_info.txt')
+except Exception as e:
+    print("An error occured:", e)
